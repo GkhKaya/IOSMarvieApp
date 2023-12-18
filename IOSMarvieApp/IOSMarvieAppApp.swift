d4b2101c4f3cb6c691f62af05b29d9249749990e
GkhKaya <gkhkaya00@gmail.com>
2023-12-18 09:57:18 +0300

    Created Project

---
+++ IOSMarvieApp/IOSMarvieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSMarvieAppApp.swift
+//  IOSMarvieApp
+//
+//  Created by Gokhan Kaya on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSMarvieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
